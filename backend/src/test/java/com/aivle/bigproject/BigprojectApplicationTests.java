309c9c1ce5760ff0a2b82e69cc8440d9d9b8db95
lselse <lselse8112@gmail.com>
2026-07-20 13:11:25 +0900

    commit

---
+++ backend/src/test/java/com/aivle/bigproject/BigprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivle.bigproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
